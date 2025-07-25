79a55a15fc915090c104d7f7b572efe59f19e558
bishoy <bishoyw.fathy@gmail.com>
2025-07-25 22:22:02 +0300

    starter code

---
+++ activity.py
@@ -0,0 +1,68 @@
+# This file is part of the Magic Moving Game.
+# Copyright (C) 2025 Bishoy Wadea
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+from sugar3.activity import activity
+from sugar3.graphics.toolbarbox import ToolbarBox
+from sugar3.activity.widgets import ActivityToolbarButton
+from sugar3.activity.widgets import StopButton
+from sugar3.graphics.toolbutton import ToolButton
+
+class MagicMoving(activity.Activity):
+    def __init__(self, handle):
+        activity.Activity.__init__(self, handle)
+        
+        # Create toolbar
+        self._create_toolbar()
+        
+        # Create a simple label
+        label = Gtk.Label()
+        label.set_markup("<span size='x-large' weight='bold'>Soma Cube</span>")
+        label.set_halign(Gtk.Align.CENTER)
+        label.set_valign(Gtk.Align.CENTER)
+        
+        # Create a main box to hold everything
+        main_box = Gtk.VBox()
+        main_box.pack_start(label, True, True, 0)
+        
+        # Add to the activity canvas
+        self.set_canvas(main_box)
+        main_box.show_all()
+    
+    def _create_toolbar(self):
+        toolbar_box = ToolbarBox()
+        
+        # Activity button
+        activity_button = ActivityToolbarButton(self)
+        toolbar_box.toolbar.insert(activity_button, -1)
+        activity_button.show()
+        
+        # Separator
+        separator = Gtk.SeparatorToolItem()
+        separator.props.draw = False
+        separator.set_expand(True)
+        toolbar_box.toolbar.insert(separator, -1)
+        
+        # Stop button
+        stop_button = StopButton(self)
+        toolbar_box.toolbar.insert(stop_button, -1)
+        stop_button.show()
+        
+        self.set_toolbar_box(toolbar_box)
+        toolbar_box.show_all()
