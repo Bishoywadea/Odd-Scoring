3fefd3a9de74b743db607b43e4172f5825dc0857
bishoy <bishoyw.fathy@gmail.com>
2025-07-25 23:09:49 +0300

    Revert "add help"
    This reverts commit 281b15a167cdd7357186e04f99e8213d7c618b51.

--- activity.py
+++ activity.py
@@ -16,20 +16,13 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 import gi
-gi.require_version("Gtk", "3.0")
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
-
-import pygame
 from sugar3.activity import activity
 from sugar3.graphics.toolbarbox import ToolbarBox
 from sugar3.activity.widgets import ActivityToolbarButton
 from sugar3.activity.widgets import StopButton
 from sugar3.graphics.toolbutton import ToolButton
-from sugar3.graphics.palette import Palette
-from gettext import gettext as _
-
-import sugargame.canvas
-from game import Game
 
 class OddScoring(activity.Activity):
     def __init__(self, handle):
@@ -39,53 +32,37 @@ class OddScoring(activity.Activity):
         self._create_toolbar()
         
         # Create a simple label
-        self.game = Game()
-
-        # Sugar Pygame canvas
-        self._pygamecanvas = sugargame.canvas.PygameCanvas(
-            self, main=self.game.run, modules=[pygame.display, pygame.font]
-        )
-        self.game.set_canvas(self._pygamecanvas)
-
-        self.set_canvas(self._pygamecanvas)
-        self._pygamecanvas.grab_focus()
+        label = Gtk.Label()
+        label.set_markup("<span size='x-large' weight='bold'>Odd Scoring Game</span>")
+        label.set_halign(Gtk.Align.CENTER)
+        label.set_valign(Gtk.Align.CENTER)
+        
+        # Create a main box to hold everything
+        main_box = Gtk.VBox()
+        main_box.pack_start(label, True, True, 0)
+        
+        # Add to the activity canvas
+        self.set_canvas(main_box)
+        main_box.show_all()
     
     def _create_toolbar(self):
         toolbar_box = ToolbarBox()
-        self.set_toolbar_box(toolbar_box)
-
+        
+        # Activity button
         activity_button = ActivityToolbarButton(self)
         toolbar_box.toolbar.insert(activity_button, -1)
-
-        help_button = ToolButton("toolbar-help")
-        help_button.set_tooltip("Help")
-        help_button.connect("clicked", self._show_help)
-        toolbar_box.toolbar.insert(help_button, -1)
-
+        activity_button.show()
+        
+        # Separator
         separator = Gtk.SeparatorToolItem()
         separator.props.draw = False
         separator.set_expand(True)
         toolbar_box.toolbar.insert(separator, -1)
-
+        
+        # Stop button
         stop_button = StopButton(self)
         toolbar_box.toolbar.insert(stop_button, -1)
-
+        stop_button.show()
+        
+        self.set_toolbar_box(toolbar_box)
         toolbar_box.show_all()
-
-    def _show_help(self, button):
-        self.game.toggle_help()
-
-    def _reset_game(self, button):
-        self.game.handle_game_over_click()
-
-    def _toggle_theme(self, button):
-        self.game.toggle_theme()
-
-    def read_file(self, file_path):
-        self.game.read_file(file_path)
-
-    def write_file(self, file_path):
-        self.game.write_file(file_path)
-
-    def close(self):
-        self.game.quit()
